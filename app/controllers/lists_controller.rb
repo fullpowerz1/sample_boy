3da152975603d1ce9f903c5503ae96b74de6cd3e
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-10-16 13:14:28 +0000

    aniki

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,6 +13,7 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
   # 以下を追加
@@ -22,6 +23,12 @@ class ListsController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # 4. 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
     redirect_to list_path(list.id)
   end
 
